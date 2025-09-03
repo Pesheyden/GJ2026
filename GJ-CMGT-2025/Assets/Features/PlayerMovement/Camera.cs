b1e1187ea7ada68cf1a23c3d6d65ca81651d34c1
philip <philip.i.simeonov@gmail.com>
2025-09-03 13:36:18 +0200

    player movement

---
+++ GJ-CMGT-2025/Assets/Features/PlayerMovement/Camera.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Transform camera;
+    public Transform target;
+    public Quaternion camRotationTo;
+    public float camRotationSpeed;
+    public float followSpeed = 5f;
+    public Vector3 offset = new Vector3(0, 2, -10);
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 desiredPosition = target.position + offset;
+        camera.position = Vector3.Lerp(transform.position, desiredPosition, followSpeed * Time.deltaTime);
+        camera.rotation = Quaternion.Lerp(camera.rotation, camRotationTo, camRotationSpeed);
+    }
+}
---
+++ GJ-CMGT-2025/Assets/Features/PlayerMovement/PlayerMovement.cs
@@ -0,0 +1,110 @@
+using UnityEngine;
+using static UnityEngine.Analytics.IAnalytic;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Transform player;
+    public float distance;
+    public float yDistance;
+    Vector3 direction;
+    bool onGRound = true;
+    public Transform camera;
+    
+    //transform.position += d * direction
+
+    void Start()
+    {
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+
+        if (Input.GetKeyDown(KeyCode.W) && noCollision(player.position, Vector3.forward, distance))
+        {
+            direction = Vector3.forward;
+            player.position += distance * direction;
+        }
+        else if (Input.GetKeyDown(KeyCode.A) && noCollision(player.position, Vector3.left, distance))
+        {
+            direction = Vector3.left;
+            if (!onGRound)
+            {
+                direction = Vector3.right;
+            }
+           
+            player.position += distance * direction;
+        }
+        else if (Input.GetKeyDown(KeyCode.S) && noCollision(player.position, Vector3.back, distance))
+        {
+            direction = Vector3.back;
+            player.position += distance * direction;
+        }
+        else if (Input.GetKeyDown(KeyCode.D) && noCollision(player.position, Vector3.right, distance))
+        {
+            direction = Vector3.right;
+            if (!onGRound)
+            {
+                direction = Vector3.left;
+            }
+           
+            player.position += distance * direction;
+        }
+        else if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (onGRound)
+            {
+                direction = Vector3.up;
+                onGRound = false;
+                
+            }
+            else
+            {
+                direction = Vector3.down;
+                onGRound = true;
+                
+            }
+
+           
+            player.position += yDistance * direction;
+            player.Rotate(0, 0, 180);
+            camera.GetComponent<Camera>().offset.y *= -1;
+            camera.GetComponent<Camera>().camRotationTo = Quaternion.Euler(camera.eulerAngles.x, camera.eulerAngles.y, camera.eulerAngles.z + 180);
+
+
+        }
+
+    }
+
+    bool noCollision(Vector3 position,Vector3 direction, float maxDistance)
+    {
+
+        if (!onGRound && (direction == Vector3.left || direction == Vector3.right))
+        {
+            direction *= -1;
+        }
+        Ray ray = new Ray(position, direction);
+        RaycastHit hit;
+        float distance;
+        if (Physics.Raycast(ray, out hit)){
+            distance = hit.distance;
+            if(distance <= maxDistance)
+            {
+                print(distance);
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+                
+        }
+        return true;
+        
+    }
+}
